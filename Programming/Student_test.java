ef1e23cde407b3a621ef1266a077a1ee0999f591
yookwangho <yoogwangho@naver.com>
2017-04-04 13:57:28 +0900

    two
    two

--- Programming/Student_test.java
+++ Programming/Student_test.java
@@ -1,6 +1,18 @@
 class Student{
-	String major;
-	Integer StudentNum;
+	private String major;
+	private Integer StudentNum;
+	public String getMajor() {
+		return major;
+	}
+	public void setMajor(String major) {
+		this.major = major;
+	}
+	public Integer getStudentNum() {
+		return StudentNum;
+	}
+	public void setStudentNum(Integer studentNum) {
+		StudentNum = studentNum;
+	}
 }
 
 
@@ -8,11 +20,11 @@ public class Student_test {
 
 	public static void main(String[] args) {
 		Student s = new Student();
-		s.major="컴퓨터정보공학과";
-		s.StudentNum=20160746;
+		s.setMajor("컴퓨터정보공학과");
+		s.setStudentNum(20160746);
 		
-		System.out.println("학과:"+s.major);
-		System.out.println("학번:"+s.StudentNum);
+		System.out.println("학과:"+s.getMajor());
+		System.out.println("학번:"+s.getStudentNum());
 	}
 
 }
